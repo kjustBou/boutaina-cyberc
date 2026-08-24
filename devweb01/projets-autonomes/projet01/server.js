cf23c41fa287a029d8b129f2b1bb24e9d7169326
kjustBou <kjustbou@gmail.com>
2026-08-24 14:24:34 -0400

    Commit du Lundi 24 août

--- devweb01/projets-autonomes/projet01/server.js
+++ devweb01/projets-autonomes/projet01/server.js
@@ -19,46 +19,143 @@ async function getApiInfo() {
         console.log(coachesData);
 
 
-        const disciplinesContainer = document.querySelector(".disciplines");
+        const disciplinesContainer = document.querySelector(".disciplines-container");
         console.log( disciplinesContainer);
-        
+
+        const coachesContainer = document.querySelector(".coaches-container");
+        console.log(coachesContainer);
+    
+        const coachIntro = document.createElement("h1");
+            coachIntro.classList.add("coach-intro");
+            coachIntro.textContent = "Come meet our team of instructors";
+            coachesContainer.appendChild(coachIntro);
+
         function afficherData(disciplines, coaches) {
 
             disciplines.forEach((discipline, index) => {
-        
+                //CARTE
                 const disciplineCard = document.createElement("div");
                 disciplineCard.classList.add("discipline-card");
                 disciplineCard.classList.add(`discipline-card-${index + 1}`);
+            
+
                 disciplinesContainer.appendChild(disciplineCard);
                 
-        
+                //Conteneur qui va tourner
+                const cardInner = document.createElement("div");
+                cardInner.classList.add("card-inner");
+
+                disciplineCard.appendChild(cardInner);
+
+                //Face avant
+                const cardFront = document.createElement("div");
+                cardFront.classList.add("card-front");
+
+                cardInner.appendChild(cardFront);
+
                 const disciplineImage = document.createElement("img");
                 disciplineImage.classList.add("discipline-image");
                 disciplineImage.classList.add(`discipline-image-${index + 1}`);
                 disciplineImage.src = discipline.img;
-                disciplineCard.appendChild(disciplineImage);
+
+                cardFront.appendChild(disciplineImage);
+
+                //Face arrière
+                const cardBack = document.createElement("div");
+                cardBack.classList.add("card-back");
+
+                cardInner.appendChild(cardBack);
 
                 const disciplineTextBox = document.createElement("div");
-                disciplineCard.appendChild(disciplineTextBox);
+                disciplineTextBox.classList.add("discipline-textBox");
+
+                cardBack.appendChild(disciplineTextBox);
 
 
                 const disciplineName = document.createElement("h2");
                 disciplineName.classList.add("discipline-name");
                 disciplineName.classList.add(`discipline-name-${index + 1}`);
-                disciplineName.innerHTML = discipline.name;
+                disciplineName.textContent = discipline.name;
 
                 disciplineTextBox.appendChild(disciplineName);
+
+                const disciplineTagline = document.createElement("h3");
+                disciplineTagline.classList.add("discipline-tagline");
+                disciplineTagline.classList.add(`discipline-tagline-${index + 1}`);
+                disciplineTagline.textContent = discipline.tagline;
+
+                disciplineTextBox.appendChild(disciplineTagline);
+
+                const disciplineDesc = document.createElement("p");
+                disciplineDesc.classList.add("discipline-desc");
+                disciplineDesc.classList.add(`discipline-desc-${index + 1}`);
+                disciplineDesc.textContent = discipline.description;
+
+                disciplineTextBox.appendChild(disciplineDesc);
+
+                disciplineCard.addEventListener("click",() => {
+                    disciplineCard.classList.toggle("flipped");
+                });
+
                 
             });
+
+        
+                
+            coaches.forEach((coach, index) => {
+
+                const coachCard = document.createElement("div");
+                coachCard.classList.add("coach-card");
+                coachCard.classList.add(`coach-card-${index + 1}`);
+
+                coachesContainer.appendChild(coachCard);
+
+                /*const coachImage = document.createElement("img");
+                coachImage.classList.add("coach-image");
+                coachImage.classList.add(`coach-image-${index +1}`);
+                coachImage.src = coach.image; 
+
+                coachCard.appendChild(coachImage);*/
+
+                const coachName = document. createElement("h3");
+                coachName.classList.add("coach-name");
+                coachName.classList.add(`coach-name-${index +1}`);
+                coachName.textContent = coach.name;
+
+                coachCard.appendChild(coachName);
+
+                const coachExperience = document.createElement("span");
+                coachExperience.textContent = `${coach.experience} years of experience`;
+
+                coachCard.appendChild(coachExperience);
+
+                const coachSession = document.createElement("span");
+                coachSession.classList.add("coach-session") ;
+                coachSession.textContent = `${coach.session} sessions`;
+                
+                coachCard.appendChild(coachSession);
+
+                const coachDiscipline = document.createElement("span");
+                coachDiscipline.classList.add("coach-discipline");
+                coachDiscipline.textContent = `Specialized in: ${coach.discipline}`;
+
+                coachCard.appendChild(coachDiscipline);
+
+            });
+
         }
-        
+    
         afficherData(disciplinesData, coachesData);
-        
+    
+   
     } catch (error) {
 
         console.error("Erreur :", error);
         alert("Erreur : " + error.message);
 
     }
-    
 }
+
+
+
+
